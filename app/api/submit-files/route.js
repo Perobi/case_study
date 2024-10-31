ecd467b6a364b5608f9715c20fdf1740274f65ae
Peribus <78774231+Perobi@users.noreply.github.com>
2024-10-31 14:38:42 +0100

    commit

--- app/api/submit-files/route.js
+++ app/api/submit-files/route.js
@@ -7,7 +7,7 @@ const TABLE_NAME = "Table 1";
 
 const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE);
 
-// Function to upload a file to File.io and get the public URL with a delay
+// Function to upload a file to File.io (free url ersteller) and get the public URL with a delay
 const uploadFileToTemporaryService = async (file) => {
   const url = "https://file.io/";
   const formData = new FormData();
@@ -16,8 +16,7 @@ const uploadFileToTemporaryService = async (file) => {
   // Introduce a delay to avoid hitting the rate limit
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  // Wait for a short period before uploading
-  await delay(500); // Adjust the delay time as necessary (500 ms as an example)
+  await delay(500); // bei 500 scheint es gut zu funktionieren
 
   const response = await fetch(url, {
     method: "POST",
@@ -26,7 +25,7 @@ const uploadFileToTemporaryService = async (file) => {
 
   const data = await response.json();
   if (data.link) {
-    return { url: data.link }; // The public URL for the uploaded file
+    return { url: data.link };
   } else {
     throw new Error("Failed to upload file: " + data.message);
   }
@@ -36,28 +35,34 @@ export async function POST(request) {
   try {
     const formData = await request.formData();
 
-    // Check for required fields
+    // Check for required fields so people can't fidget around with the form elements
     const userEmail = formData.get("userEmail");
     const userFirstName = formData.get("userFirstName");
     const userLastName = formData.get("userLastName");
     const userPhone = formData.get("userPhone");
 
-    if (!userEmail || !userFirstName || !userLastName || !userPhone) {
+    // Get files for objektfotos and unterlagen
+    const objektfotos = formData.getAll("objektFotos");
+    const unterlagen = formData.getAll("gebaudeUnterlagen");
+
+    if (
+      !userEmail ||
+      !userFirstName ||
+      !userLastName ||
+      !userPhone ||
+      (objektfotos.length === 0 && unterlagen.length === 0)
+    ) {
       return NextResponse.json(
         {
           success: false,
           error:
             "Bitte stellen Sie sicher, dass alle erforderlichen Felder ausgefüllt sind.",
         },
-        { status: 400 } // Bad Request status
+        { status: 400 } // Bad Request status! No no not allowed sneaky sneaky
       );
     }
 
-    // Get separate files for objektfotos and unterlagen
-    const objektfotos = formData.getAll("objektFotos");
-    const unterlagen = formData.getAll("gebaudeUnterlagen");
-
-    // Upload all objektfotos and unterlagen files
+    // Upload all objektfotos and unterlagen
     const uploadedObjektfotos = await Promise.all(
       objektfotos.map((file) => uploadFileToTemporaryService(file))
     );
